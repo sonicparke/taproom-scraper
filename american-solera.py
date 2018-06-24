bf9b541d4dbbe1be75886e0c2e6d9b46b17602cc
Brad McAlister <brad.mcalister@vitals.com>
2018-06-23 19:40:15 -0500

    finally got AS split up into 2 different arrays

--- american-solera.py
+++ american-solera.py
@@ -7,59 +7,50 @@ response = get(url)
 html_soup = BeautifulSoup(response.text, 'html.parser')
 pTag = html_soup.find_all('p')
 extracted_records = []
+location1 = []
+location2 = []
 taproom = ''
 last_taproom = 'none'
 beer = ''
 
+# Remove search text at end of page
 search_text = html_soup.findAll("p", class_="search-text")
 for search in search_text:
-  search.extract()
+    search.decompose()
 
-
+# Look at all the <p> tags
 for tag in pTag:
-  spanTag = tag.find('span')
-  if spanTag:
-    strongTag = spanTag.find('strong')
+
+    # If there is a span it's probably a taproom location
+    spanTag = tag.find('span')
+    if spanTag:
+        strongTag = spanTag.find('strong')
+        if strongTag:
+            taproom = strongTag.text.strip()
+            if not location1:
+                location1.append(taproom)
+                # print('location1: ', location1)
+
+            else:
+                location2.append(taproom)
+                # print('location2: ', location2)
+
+    # If there is a <strong> tag but not inside a <span> then it's probably a column heading
+    strongTag = tag.find('strong')
     if strongTag:
-      taproom = strongTag.text.strip()
-      extracted_records = {
-        'location': taproom
-      }
-  strongTag = tag.find('strong')
-  if strongTag:
-    parent = strongTag.find_parent('p')
-    parent.decompose()
-  if (last_taproom != taproom):
-    print('taproom: ', taproom)
-  else:
-    print('tag: ', tag)
+        parent = strongTag.find_parent('p')
+        parent.decompose()
+
+    # Grab the beers and descriptions
+    else:
+        if not location1 or not location2:
+            location1.append(tag.text.strip())
+        else:
+            location2.append(tag.text.strip())
 
 
-  if last_taproom != taproom:
-    last_taproom = taproom
-    # while last_taproom == taproom:
-    # print('last_taproom: ', last_taproom)
-    # print('tag: ', tag)
-    # Filter out Headers
-    if tag.find('strong'):
-      tag.extract()
+print('location1: ', location1)
+print('location2: ', location2)
 
-
-
-    # else:
-    #   left_column = tag.parent.parent.parent.parent.find_all('div', class_='vc_col-has-fill')
-
-    #   for div in left_column:
-
-    #     found_beer = [x for x in div.div.div.div.div if x.name == 'p']
-
-    #     # beer = found_beer[0].text.strip()
-    #     print('found_beer: ', found_beer)
-
-    #   beers = {
-    #     'beer_name': beer
-    #   }
-      # print('BEER: ', beer)
-
-
+# print('extracted_records: ', extracted_records)
 print('done')
