0b64e4dc5ffa001b16493ff7a86fc41f7080b540
Brad McAlister <brad.mcalister@vitals.com>
2018-06-24 01:24:24 -0500

    updating heirloom

--- heirloom.py
+++ heirloom.py
@@ -12,8 +12,8 @@ extracted_records = []
 for beer in beers:
   if not beer.text.isspace() and not beer.find_previous_sibling('h3'):
     record = {
-      'beer_name': beer.text,
-      'description': beer.find_next_sibling('p').text
+      "beer": beer.text,
+      "description": beer.find_next_sibling('p').text
     }
 
     extracted_records.append(record)
